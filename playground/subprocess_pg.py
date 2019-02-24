3894373c8d6bf41b97e759a065cd5b316fbf1bb5
Brando Miranda <brando90@mit.edu>
2019-02-24 13:30:26 -0600

    progress with subprocess module

--- playground/subprocess_pg.py
+++ playground/subprocess_pg.py
@@ -1,18 +1,59 @@
 import subprocess
 
-def attempt1():
-    #p = subprocess.Popen(['rlwrap', 'sertop', '--printer=human'])
-    p = subprocess.Popen(['sertop'])
-    #print(p)
-    #print(f'type(p)={type(p)}')
-    #print(f'p={p}')
+def simple():
+    # Simple command
+    #output = subprocess.call(['ls', '-1'], shell=True)
+    output = subprocess.check_output(['ls', '-1'])
+    print(f'output: {output}')
+    ##
+    #output = subprocess.call('echo $HOME', shell=True)
+    #print(f'output echo $HOME = {output}')
+
+def popen_pg():
+    ##
+    proc = subprocess.Popen(['echo', '"to stdout"'],
+                        stdout=subprocess.PIPE,
+                        stdin=subprocess.PIPE
+                        )
+    print(proc)
+    ##
+    stdout_value = proc.communicate()[0]
+    print('\tstdout:', repr(stdout_value))
+
+
+def serapi_attempt1():
+    ##
+    proc = subprocess.Popen(['sertop'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE,)
+    #proc = subprocess.Popen(['sertop'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.STDOUT,)
+    print(proc)
+    comm = proc.communicate()
+    print('\n---information about calling comm')
+    print(type(comm))
+    print(len(comm))
+    print('\n---stdout_data')
+    print(comm[0])
+    print(type(comm[0]))
+    print('\n---stderror_data')
+    print(comm[1])
+    print(type(comm[0]))
+    ##
+    #stdout_value = proc.communicate()[0]
+    #print('\tstdout:', repr(stdout_value))
 
 def context_manager():
-    with subprocess.Popen(['sertop']):
-        print(f'type(p)={type(p)}')
-        print(f'p={p}')
+    with subprocess.Popen(['sertop'],stdout=subprocess.PIPE) as proc:
+        print(f'type(proc)={type(proc)}')
+        print(f'proc={proc}')
+        #stdout_value = proc.communicate()[0]
+        #print('\tstdout:', repr(stdout_value))
 
 if __name__ == '__main__':
-    attempt1()
+    print('running main')
+    #attempt1()
+    #context_manager()
+    #popen_pg()
+    serapi_attempt1()
 
 #(Sys_error"Input/output error")
+##
+print('end of main')
