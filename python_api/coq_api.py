51b818219d000c0a43df449939a0a40ef204b1d3
Brando Miranda <brando90@mit.edu>
2019-03-06 14:13:19 -0600

    added the current way to add comments and documentation to code

--- python_api/coq_api.py
+++ python_api/coq_api.py
@@ -1,32 +1,51 @@
 import subprocess
 
-class Coq:
-    def __init__(self, debug=False):
-        '''
-        1) run a serapi instance in the background
-        2) send Coq commands to the runnign instance of serapi
-        3) get responses and do stuff
+import pdb
 
+class Coq:
+    # docstrings according to https://sphinxcontrib-napoleon.readthedocs.io/en/latest/example_google.html
+    # function_with_types_in_docstring
+    def __init__(self, debug=False):
+        '''Constructor for the Coq API class that talks to Coq.
+
+        1) run a serapi instance in the background
+        2) send Coq commands to the running instance of serapi
+        3) get responses and do stuff
         Protocol mli file: https://github.com/brando90/coq-serapi-python/blob/python-api/serapi/serapi_protocol.mli
+
+        Args:
+            debug (bool): True when in debugging state.
         '''
         ## start serapi, TODO: --no_init flag, think about how to use feedback for ML better
         self.serapi = subprocess.Popen(['sertop','--no_init'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
         self.debug = debug
 
     def run_command(self, cmd):
-        ## send command
+        ''' Runs the raw command command cmd given.
+
+        Args:
+            cmd (str): command as a string.
+        '''
+        ## send command fully to serapi
         self._send_command(cmd)
         ## get results of command
-        results = self._get_results()
-        return results
+        result = self._get_result()
+        return result
 
     ## COMMANDS: https://github.com/brando90/coq-serapi-python/blob/d394050372b6ab680fda2680d6e4ba53fdabb875/serapi/serapi_protocol.mli#L212
 
     def new_doc(self, cmd):
+        '''
+        TODO
+        '''
         return
 
     def add(self, text, tag=None):
-        '''
+        ''' Executes the Add command with the given text (and tag).
+
+        Args:
+            text (str): text for Coq proof script.
+            tag (text): text for tag.
         '''
         cmd = f"( Add () \"{text}\")"
         if tag:
@@ -36,23 +55,27 @@ class Coq:
         return result
 
     def _send_command(self,cmd):
-        '''
+        ''' Sends command to be executed to serapi.
 
-        e.g.
-            p.stdin.write(b'(Add () \"Example test_oddb1: Nat.odd 1 = true.\")\n')
-            p.stdin.flush() # it pushes things to the actual file rather than keep it in the buffer
+        Args:
+            cmd (str): command text for serapi. e.g. cmd = '( Add () "Example test_oddb1: Nat.odd 1 = true.")'
         '''
         cmd = bytes(cmd,'utf-8')
-        #print(f'cmd={cmd}')
+        # send command to serapi
         self.serapi.stdin.write(cmd)
+        # pushes things to the actual file serapi reads rather than keep it in the buffer
         self.serapi.stdin.flush()
 
-    def _get_results(self):
-        '''
+    def _get_result(self):
+        ''' Get the result from the last command exectued.
+
+        Args:
+            result (list): list of s-expressions from the command result.
         '''
         ## process result of command
         completed_getting_results = False
         result = []
+        # extract the whole command result from the buffer (separated by \n)
         while not completed_getting_results: # while searching for completed tag command
             line = self.serapi.stdout.readline()
             if self.debug:
@@ -66,7 +89,14 @@ class Coq:
 ## utils
 
 def add_tag(tag,cmd):
-    return f"( {tag} {cmd} )"
+    ''' Adds the given tag to the command.
+
+    Args:
+        tag (str): tag for command e.g. 'tag1' in '( tag1 (Add () "Example test_oddb1: Nat.odd 1 = true.") )'
+        cmd (str): coq serapi command '(Add () "Example test_oddb1: Nat.odd 1 = true.")'
+    '''
+    tagged_command = f"( {tag} {cmd} )"
+    return tagged_command
 
 def pythonize_sexpt(sexpt):
     '''
---
+++ python_api/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='python_api',
+      version='0.0.1',
+      # And any other dependencies foo needs
+      install_requires=['sexpdata']
+)
