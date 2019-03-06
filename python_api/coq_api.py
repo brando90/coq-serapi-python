856abb29e39cfef8775ad49ff299e752f58e90cb
Brando Miranda <brando90@mit.edu>
2019-03-05 20:59:48 -0600

    first sync coq prototpye

--- python_api/coq_api.py
+++ python_api/coq_api.py
@@ -1,7 +1,7 @@
 import subprocess
 
-class coq:
-    def __init__(self):
+class Coq:
+    def __init__(self, debug=False):
         '''
         1) run a serapi instance in the background
         2) send Coq commands to the runnign instance of serapi
@@ -10,39 +10,58 @@ class coq:
         Protocol mli file: https://github.com/brando90/coq-serapi-python/blob/python-api/serapi/serapi_protocol.mli
         '''
         ## start serapi, TODO: --no_init flag, think about how to use feedback for ML better
-        self.serapi = subprocess.Popen(['sertop','--no_init'],
-                                    stdin=subprocess.PIPE,
-                                    stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE)
+        self.serapi = subprocess.Popen(['sertop','--no_init'],stdin=subprocess.PIPE,stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+        self.debug = debug
 
-  def run_command(cmd):
-      '''
-      TODO
-      '''
-      return
-
-  def newdoc(self, action):
-      '''
-      TODO
-      '''
-
-  def add(self, text, tag=None):
-      '''
-      '''
-      cmd = f" (Add () \"{text}\") "
-      if tag:
-          cmd = add_tag(tag,cmd)
-
-        p.stdin.write(b)
-        p.stdin.flush() # it pushes things to the actual file rather than keep it in the buffer
+    def run_command(self, cmd):
+        ## send command
+        self._send_command(cmd)
+        ## get results of command
+        results = self._get_results()
         return
 
+    def new_doc(self, cmd):
+        return
 
+    def add(self, cmd, tag=None):
+        cmd = f"( Add () \"{text}\")"
+        if tag:
+            cmd = add_tag(cmd)
+        ##
+        result = self.run_command(cmd)
+        return
+
+    def _send_command(cmd):
+        '''
+        '''
+        self.stdin.write(cmd)
+        self.stdin.flush(cmd)
+
+    def _get_results(self):
+        '''
+        '''
+        ## process result of command
+        completed_getting_results = False
+        results = []
+        while not completed_getting_results: # while searching for completed tag command
+            line = self.serapi.stdout.readline()
+            if self.debug:
+                print(f'line={line}')
+            current_result = pythonize_sexpt(sexpt=line)
+            results.append(current_result)
+            ## if complete tag found then we don't need to keep reading from serapi
+            completed_getting_results = 'Completed' in line
+        return results
 
 ## utils
 
 def add_tag(tag,cmd):
     return f"( {tag} {cmd} )"
 
+def pythonize_sexpt(sexpt):
+    '''
+        TODO: convert sexpt to python object
+    '''
+    return str(sexpt)
 
 if __name__ == '__main__':
--- python_api/coq_api_tests.py
+++ python_api/coq_api_tests.py
@@ -3,6 +3,7 @@ import unittest
 import coq_api
 
 class TestStringMethods(unittest.TestCase):
+    # example test: https://github.com/brando90/eit_proj1/blob/master/main_proj_lib/tests/test_user_implemented.py
 
     def __init__(self):
         self.coq = coq_api.coq()
